b8ab6309c300246d0719653f2e01b8f7b725f7a7
Andy <84168944+AZurek17@users.noreply.github.com>
2023-11-28 12:16:19 -0700

    webpack.config and packagejson updated

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,3 +1,6 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { InjectManifest } = require('workbox-webpack-plugin');
 const path = require('path');
 
 module.exports = () => {
@@ -11,16 +14,46 @@ module.exports = () => {
 
     // TODO: Add the correct output
     output: {
-      
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'index.js'
+  
     },
 
     // TODO: Add the correct plugins
     plugins: [
-     
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'webpack Plugin',
+      }),
+      new MiniCssExtractPlugin(),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'sw.js',
+      }),
     ],
 
     // TODO: Add the correct modules
     module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },   
+      ],
 
     }
   };
